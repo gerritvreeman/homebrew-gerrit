40053e967c2d2cba5c58841a321efedea33f3b25
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2022-07-10 13:31:56 -0500

    added olex2

--- Casks/mnova.rb
+++ Casks/mnova.rb
@@ -1,7 +1,7 @@
 cask "mnova" do
   version "14.2.2"
   sha256 "3cebb8738e84ee4775ca3130abe8ce5956e900aaf80c45d0fc5f80256216306f"
-  
+
   url "https://mestrelab.com/downloads/mnova/mac/MestReNova-14.2.2-28739.dmg"
   name "MNova"
   desc "A top class software suite to process your analytical chemistry data"
---
+++ Casks/olex2.rb
@@ -0,0 +1,11 @@
+cask "olex2" do
+  version "1.5_6380"
+  sha256 "819d0e06b9b51c91e8fcbc3eaaa0b31a304424bed419479cdae1e487e0f69e2b"
+
+  url "www.olex2.org/olex2-distro/olex2-1.5.app.dmg"
+  name "olex2-1.5"
+  desc " An easy-to-use program containing everything you need to solve, refine and finish small-molecule crystal structures using an intuitive user interface."
+  homepage "https://www.olexsys.org/olex2"
+
+  app "olex2-1.5.app"
+end
