6806dfe3cff521a76ef2ed7a2908015b1f969384
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2022-07-11 11:19:34 -0500

    maybe fixed colorpallet again? v2

--- Casks/colorpicker-umn.rb
+++ Casks/colorpicker-umn.rb
@@ -1,6 +1,6 @@
 cask "colorpicker-umn" do
   version "0.0.1"
-  sha256 :no_check
+  sha256 "fc7880759c03fa6b9b12948fc6896b146c0c2ac1922c2aba119acfdc00d7ff14"
 
   url "https://github.com/gerritvreeman/homebrew-gerrit/tree/main/assets/colors.zip"
   name "UMN colorpicker"
